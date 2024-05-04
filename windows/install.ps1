af82ff4435e29010fb7a61ec90516c58ca3bbe4d
Thomas Leroy <thomas@agence-bmobile.com>
2024-05-04 19:55:15 +0200

    fix: follow PowerShell best practices
    Signed-off-by: Thomas Leroy <5416321+thomas-leroy@users.noreply.github.com>

--- windows/install.ps1
+++ windows/install.ps1
@@ -8,10 +8,10 @@ function Set-Color {
     Process {
         if ($PSCmdlet.ShouldProcess("Setting terminal color to $color")) {
             switch ($color) {
-                'green'  { Write-Host -ForegroundColor Green }
-                'red'    { Write-Host -ForegroundColor Red }
-                'yellow' { Write-Host -ForegroundColor Yellow }
-                default  { Write-Host -ForegroundColor White }
+                'green'  { Write-Information -MessageData "Color set to green" -InformationAction Continue }
+                'red'    { Write-Information -MessageData "Color set to red" -InformationAction Continue }
+                'yellow' { Write-Information -MessageData "Color set to yellow" -InformationAction Continue }
+                default  { Write-Information -MessageData "Color set to white" -InformationAction Continue }
             }
         }
     }
@@ -23,7 +23,7 @@ function Install-Chocolatey {
     param ()
     if ($PSCmdlet.ShouldProcess("Installing Chocolatey")) {
         Set-Color -color "yellow"
-        Write-Host "Chocolatey not found. Installing Chocolatey..."
+        Write-Information "Chocolatey not found. Installing Chocolatey..."
         $chocoInstallScriptUrl = 'https://chocolatey.org/install.ps1'
         $chocoInstallScriptPath = Join-Path -Path $env:TEMP -ChildPath "chocoInstall.ps1"
         Invoke-WebRequest -Uri $chocoInstallScriptUrl -OutFile $chocoInstallScriptPath
@@ -39,39 +39,38 @@ function Install-Make {
     param ()
     if ($PSCmdlet.ShouldProcess("Installing make utility via Chocolatey")) {
         Set-Color -color "yellow"
-        Write-Host "Installing make utility..."
+        Write-Information "Installing make utility..."
         choco install make -y
     }
 }
 
-# Check if WSL is installed and handle installation processes
-function Check-And-Install-WSL {
+# Install or Verify Windows Subsystem for Linux (WSL) and Ubuntu
+function Install-WSL {
     [CmdletBinding(SupportsShouldProcess=$true)]
     param ()
-    Set-Color -color "yellow"
-    Write-Host "Checking for Windows Subsystem for Linux (WSL)..."
-    $wsl = wsl --list --quiet
-    if ($LASTEXITCODE -eq 0) {
-        if ($PSCmdlet.ShouldProcess("WSL detected, checking for Ubuntu")) {
+    if ($PSCmdlet.ShouldProcess("Installing or verifying Windows Subsystem for Linux (WSL)")) {
+        Set-Color -color "yellow"
+        Write-Information "Checking for Windows Subsystem for Linux (WSL)..."
+        $wsl = wsl --list --quiet
+        if ($LASTEXITCODE -eq 0) {
             Set-Color "green"
             $wslVersion = wsl --status | Select-String "Version" | Out-String
-            Write-Host "WSL is installed. Version:" $wslVersion.Trim()
-
+            Write-Information "WSL is installed. Version: $($wslVersion.Trim())"
             $ubuntu = wsl -l -v | Select-String "Ubuntu"
             if (-Not $ubuntu) {
-                if ($PSCmdlet.ShouldProcess("Ubuntu not found, prompting to install")) {
+                if ($PSCmdlet.ShouldProcess("Ubuntu not found on WSL, prompt to install")) {
                     Set-Color "red"
-                    Write-Host "Ubuntu is not installed on WSL. Please install Ubuntu from the Microsoft Store."
+                    Write-Information "Ubuntu is not installed on WSL. Please install Ubuntu from the Microsoft Store."
                 }
             }
-        }
-    } else {
-        if ($PSCmdlet.ShouldProcess("WSL is not installed, initiating installation")) {
-            Set-Color "red"
-            Write-Host "WSL is not installed. Installing WSL..."
-            dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-            Set-Color "yellow"
-            Write-Host "Please reboot your system and install Ubuntu from the Microsoft Store to complete the WSL setup."
+        } else {
+            if ($PSCmdlet.ShouldProcess("WSL is not installed, initiating installation")) {
+                Set-Color "red"
+                Write-Information "WSL is not installed. Installing WSL..."
+                dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+                Set-Color "yellow"
+                Write-Information "Please reboot your system and install Ubuntu from the Microsoft Store to complete the WSL setup."
+            }
         }
     }
 }
@@ -82,19 +81,19 @@ function Install-VSCode {
     param ()
     if ($PSCmdlet.ShouldProcess("Installing Visual Studio Code")) {
         Set-Color -color "yellow"
-        Write-Host "Installing Visual Studio Code..."
+        Write-Information "Installing Visual Studio Code..."
         choco install vscode -y
         Set-Color "green"
-        Write-Host "Visual Studio Code installed successfully!"
+        Write-Information "Visual Studio Code installed successfully!"
     }
 }
 
 # Execute the script
 Install-Chocolatey
 Install-Make
-Check-And-Install-WSL
+Install-WSL
 Install-VSCode
 
 # Reset color
 Set-Color "white"
-Write-Host "Setup completed successfully!"
+Write-Information "Setup completed successfully!"
