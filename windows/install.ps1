e037466a18f44063950a18535d28b30b5173fa52
Thomas Leroy <thomas@agence-bmobile.com>
2024-05-04 19:32:36 +0200

    refactor: Avoid using Write-Host instead of Write-Output
    Signed-off-by: Thomas Leroy <5416321+thomas-leroy@users.noreply.github.com>

--- windows/install.ps1
+++ windows/install.ps1
@@ -4,20 +4,20 @@ function Set-Color {
         [String]$color
     )
     if ($color -eq "green") {
-        Write-Host -ForegroundColor Green
+        Write-Output -ForegroundColor Green
     } elseif ($color -eq "red") {
-        Write-Host -ForegroundColor Red
+        Write-Output -ForegroundColor Red
     } elseif ($color -eq "yellow") {
-        Write-Host -ForegroundColor Yellow
+        Write-Output -ForegroundColor Yellow
     } else {
-        Write-Host -ForegroundColor White
+        Write-Output -ForegroundColor White
     }
 }
 
 # Install Chocolatey if not already installed (Required to install 'make' and Visual Studio Code)
 if (-Not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Set-Color "yellow"
-    Write-Host "Chocolatey not found. Installing Chocolatey..."
+    Write-Information "Chocolatey not found. Installing Chocolatey..."
     # New method to install Chocolatey
     $chocoInstallScriptUrl = 'https://chocolatey.org/install.ps1'
     $chocoInstallScriptPath = Join-Path -Path $env:TEMP -ChildPath "chocoInstall.ps1"
@@ -29,42 +29,42 @@ if (-Not (Get-Command choco -ErrorAction SilentlyContinue)) {
 
 # Install make using Chocolatey
 Set-Color "yellow"
-Write-Host "Installing make utility..."
+Write-Information "Installing make utility..."
 choco install make -y
 
 # Check if WSL is installed
 Set-Color "yellow"
-Write-Host "Checking for Windows Subsystem for Linux (WSL)..."
+Write-Information "Checking for Windows Subsystem for Linux (WSL)..."
 $wsl = wsl --list --quiet
 if ($LASTEXITCODE -eq 0) {
     # WSL is installed, get version
     Set-Color "green"
     $wslVersion = wsl --status | Select-String "Version" | Out-String
-    Write-Host "WSL is installed. Version:" $wslVersion.Trim()
+    Write-Information "WSL is installed. Version:" $wslVersion.Trim()
     
     # Check for Ubuntu installation in WSL
     $ubuntu = wsl -l -v | Select-String "Ubuntu"
     if (-Not $ubuntu) {
         Set-Color "red"
-        Write-Host "Ubuntu is not installed on WSL. Please install Ubuntu from the Microsoft Store."
+        Write-Information "Ubuntu is not installed on WSL. Please install Ubuntu from the Microsoft Store."
     }
 } else {
     # WSL is not installed, install WSL
     Set-Color "red"
-    Write-Host "WSL is not installed. Installing WSL..."
+    Write-Information "WSL is not installed. Installing WSL..."
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
     # Inform the user to reboot and install Ubuntu from the store
     Set-Color "yellow"
-    Write-Host "Please reboot your system and install Ubuntu from the Microsoft Store to complete the WSL setup."
+    Write-Information "Please reboot your system and install Ubuntu from the Microsoft Store to complete the WSL setup."
 }
 
 # Install Visual Studio Code using Chocolatey
 Set-Color "yellow"
-Write-Host "Installing Visual Studio Code..."
+Write-Information "Installing Visual Studio Code..."
 choco install vscode -y
 
 Set-Color "green"
-Write-Host "Setup completed successfully!"
+Write-Information "Setup completed successfully!"
 
 # Reset color
 Set-Color "white"
