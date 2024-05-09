0f61102b939511e22823fdb5a7afb1bae5cc38eb
Thomas Leroy <5416321+thomas-leroy@users.noreply.github.com>
2024-05-09 18:58:40 +0200

    feat: add other aliases for zsh
    Signed-off-by: Thomas Leroy <5416321+thomas-leroy@users.noreply.github.com>

---
+++ zsh/pubkey.zsh
@@ -0,0 +1,5 @@
+# Inspired by holman dotfile 
+# https://github.com/holman/dotfiles
+
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
